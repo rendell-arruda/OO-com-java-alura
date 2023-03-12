18ff2f37573a98e3a4eb127e2b927524a9a409d2
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-12 15:01:11 -0300

    aula de inteiros e double

---
+++ java-JRE-e-JDK-compile/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,12 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste");
+		
+		 int idade;
+		 idade = 31;
+		 System.out.println(idade);
+		 
+	}
+
+}
