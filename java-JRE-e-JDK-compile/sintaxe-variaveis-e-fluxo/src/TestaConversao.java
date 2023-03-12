da59d241eb1ec185c4622a5a4b0a4401179a219e
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-12 16:36:15 -0300

    variaveis, char, String e concatenacao

---
+++ java-JRE-e-JDK-compile/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,12 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		double salario = 1274.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+	
+
+	}
+}
