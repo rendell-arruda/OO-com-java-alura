281df2412b7a4aaaf32967665201680b4ba6210a
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-12 17:22:14 -0300

    condicionais if else e escopo

--- java-JRE-e-JDK-compile/sintaxe-variaveis-e-fluxo/src/TesteIR.java
+++ java-JRE-e-JDK-compile/sintaxe-variaveis-e-fluxo/src/TesteIR.java
@@ -2,16 +2,17 @@
 public class TesteIR {
 	public static void main(String[] args) {
 
-		double salario = 3300.0;
+		double salario = 3751.1;
 
-		if (salario < 2600.0) {
+		if (salario <= 2800.0) {
+		    System.out.println("A sua aliquota é de 7,5%");
+            System.out.println("Você pode deduzir até R$ 142");
+		} else if (salario >2800 && salario <= 3751.0) {
 			System.out.println("A sua aliquota é de 15%");
-			System.out.println("Você pode deduzir até R$ 350");
-		}
-
-		if (salario < 3750.0) {
-			System.out.println("A sua aliquota é de 22,5%");
-			System.out.println("Você pode deduzir até R$ 636");
+            System.out.println("Você pode deduzir até R$ 350");
+		}else if (salario > 3751.0 && salario <= 4664.0) {
+			System.out.println("A sua aliquota é de 22.5%");
+            System.out.println("Você pode deduzir até R$ 636");
 		}
 	}
 }
