351ad640dad9922d36f84ab495a1071040169848
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-13 15:30:28 -0300

    classes, objetos e referencias

---
+++ 02-entendendo-a-OO/bytebank/src/TesteDeReferencia.java
@@ -0,0 +1,12 @@
+
+public class TesteDeReferencia {
+		public static void main(String[] args) {
+			Conta primeiraConta = new Conta();
+			primeiraConta.saldo = 300;
+			
+			
+			Conta segundaConta = primeiraConta;
+			
+		}
+
+}
