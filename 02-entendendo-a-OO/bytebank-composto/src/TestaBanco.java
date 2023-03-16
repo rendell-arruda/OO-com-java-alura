501389f6ef7392b550096ba329a4496a065e513f
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-16 13:01:26 -0300

    referencia conta com cliente

--- 02-entendendo-a-OO/bytebank-composto/src/TestaBanco.java
+++ 02-entendendo-a-OO/bytebank-composto/src/TestaBanco.java
@@ -9,7 +9,10 @@ public class TestaBanco {
 		Conta contaDoPaulo = new Conta();
 		contaDoPaulo.deposita(100);
 		
+		
+		//associa o cliente paulo a contaDoPaulo
 		contaDoPaulo.titular = paulo;
+		
 		System.out.println(contaDoPaulo.titular.nome);
 		System.out.println(contaDoPaulo.titular);
 		System.out.println(paulo);
--- 02-entendendo-a-OO/bytebank-composto/src/TestaContaSemCliente.java
+++ 02-entendendo-a-OO/bytebank-composto/src/TestaContaSemCliente.java
@@ -2,6 +2,14 @@
 public class TestaContaSemCliente {
 	public static void main(String[] args) {
 		Conta contaDaMarcela = new Conta();
+		System.out.println(contaDaMarcela.saldo);
+		
+		contaDaMarcela.titular = new Cliente();
+		//comecou a popular a conta
+		
+		contaDaMarcela.titular.nome = "Marcela";
+		
+		System.out.println(contaDaMarcela.titular.nome);
 		
 		
 	}
