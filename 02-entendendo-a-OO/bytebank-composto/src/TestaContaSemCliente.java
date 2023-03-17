b9b21e708054335201656119f663b1ac355975e2
Rendell Arruda <rendellarruda3@gmail.com>
2023-03-17 18:23:42 -0300

    getters e setters

--- 02-entendendo-a-OO/bytebank-composto/src/TestaContaSemCliente.java
+++ 02-entendendo-a-OO/bytebank-composto/src/TestaContaSemCliente.java
@@ -2,7 +2,7 @@
 public class TestaContaSemCliente {
 	public static void main(String[] args) {
 		Conta contaDaMarcela = new Conta();
-		System.out.println(contaDaMarcela.pegaSaldo());
+		System.out.println(contaDaMarcela.getSaldo());
 		
 		contaDaMarcela.titular = new Cliente();
 		//comecou a popular a conta
--- java-JRE-e-JDK-compile/bytebank-encapsulado/src/Conta.java
+++ java-JRE-e-JDK-compile/bytebank-encapsulado/src/Conta.java
@@ -1,8 +1,8 @@
 public class Conta {
 	private double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
 
 	void deposita(double valor) {
 		this.saldo += valor;
@@ -30,5 +30,29 @@ public class Conta {
 		return this.saldo;
 	}
 	
+	public int getNumero() {
+		return this.numero;
+	}
+	
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public int getAgencia() {
+		return this.agencia;
+	}
+	
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return titular;
+	}
 	
 }
